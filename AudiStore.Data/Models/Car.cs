0966da7ec275bbd10209242424e280611ab4a676
babaika.belarus <babaika.belarus@gmail.com>
2020-03-14 18:22:25 +0300

    add Key attribute for ID

--- AudiStore.Data/Models/Car.cs
+++ AudiStore.Data/Models/Car.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AudiStore.DAL.Models
@@ -6,6 +7,7 @@ namespace AudiStore.DAL.Models
     public class Car
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public Guid Id { get; set; }
         public string Name { get; set; }
     }
